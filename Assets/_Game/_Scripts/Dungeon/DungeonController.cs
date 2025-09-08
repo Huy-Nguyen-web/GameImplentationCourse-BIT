f7db74eda4f569a0cccc35f02c446a0a312095e7
huy ngyen <huy.nguyentrong2000@gmail.com>
2025-09-08 10:31:39 +0300

    (~) Fix some tilemap functions

--- Assets/_Game/_Scripts/Dungeon/DungeonController.cs
+++ Assets/_Game/_Scripts/Dungeon/DungeonController.cs
@@ -5,46 +5,35 @@ using UnityEngine;
 
 public class DungeonController : Singleton<DungeonController>
 {
-    public enum DungeonRoomType
-    {
-        None,
-        T, // Top
-        B, // Bottom
-        L, // Left
-        R, // Right
-        TL, // Top Left
-        TR, // Top Right
-        BL, // Bottom Left
-        BR, // Bottom Right
-        TBL, // Top Bottom Left
-        TBR, // Top Bottom Right
-        TLR, // Top Left Right
-        BLR, // Bottom Left Right
-        TBLR, // Top Bottom Left Right
-    }
-
     private enum MoveOptions
     {
         Left,
         Right,
         Bottom,
     }
-    
+
     [Header("Setup")]
-    [SerializedDictionary] public SerializedDictionary<DungeonRoomType, DungeonRoom> roomsPrefab;
+    [SerializeField] private DungeonRoom roomPrefab;
     [SerializeField] private Vector2Int dungeonSize;
 
     private readonly DungeonRoom[,] _rooms = new DungeonRoom[4, 4];
 
-    public DungeonRoom SpawnRoom(DungeonRoomType dungeonRoomType)
+    public DungeonRoom SpawnRoom(Vector2Int position,
+        bool left = false,
+        bool right = false,
+        bool bottom = false,
+        bool top = false)
     {
-        DungeonRoom room = Instantiate(roomsPrefab[dungeonRoomType], transform);
+        DungeonRoom room = Instantiate(roomPrefab, transform);
+        Debug.Log($"Spawn room at {position}");
         return room;
     }
 
-    private void GenerateRoom(Vector2Int currentPos, MoveOptions previousMove, bool isMakingPath, bool isDone)
+    private void GenerateRoom()
     {
-        if (isMakingPath)
+        bool done = false;
+        Vector2Int currentPos = new Vector2Int(Random.Range(0, dungeonSize.x), Random.Range(0, dungeonSize.y));
+        while (!done)
         {
             List<MoveOptions> moves = new List<MoveOptions>()
             {
@@ -53,29 +42,35 @@ public class DungeonController : Singleton<DungeonController>
                 MoveOptions.Bottom,
             };
 
-            if (currentPos.x == 0 || previousMove == MoveOptions.Right) moves.Remove(MoveOptions.Left);
-            if (currentPos.x == dungeonSize.x - 1 || previousMove == MoveOptions.Left) moves.Remove(MoveOptions.Right);
+            if (currentPos.x == 0) moves.Remove(MoveOptions.Left);
+            if (currentPos.x == dungeonSize.x - 1) moves.Remove(MoveOptions.Right);
             
             var currentMove = moves[Random.Range(0, moves.Count)];
-            Vector2Int nextPos;
             
-            _rooms[currentPos.x, currentPos.y] = SpawnRoom(DungeonRoomType.B);
+            _rooms[currentPos.x, currentPos.y] = SpawnRoom(currentPos);
             switch (currentMove)
             {
                 case MoveOptions.Left:
-                    nextPos = new Vector2Int(currentPos.x - 1, currentPos.y);
+                    currentPos = new Vector2Int(currentPos.x - 1, currentPos.y);
                     break;
                 case MoveOptions.Right:
-                    nextPos = new Vector2Int(currentPos.x + 1, currentPos.y);
+                    currentPos = new Vector2Int(currentPos.x + 1, currentPos.y);
                     break;
                 case MoveOptions.Bottom:
+                    if (currentPos.y == dungeonSize.y - 1)
+                    {
+                        done = true;
+                        // TODO: Generate the final room here
+                        // TODO: Generate the door
+                        return;
+                    }
+                    currentPos = new Vector2Int(currentPos.x, currentPos.y + 1);
+                    break;
+                default:
+                    Debug.LogError("Something goes wrong");
+                    done = true;
                     break;
             }
-        }
-        
-        else
-        {
-            
         }
     }
 }
