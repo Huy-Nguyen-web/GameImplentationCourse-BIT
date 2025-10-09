5d20b0873aed63963f6df77007dac92edc5743e5
Mukluk10 <pyry.urala@gmail.com>
2025-10-09 09:56:48 +0300

    enemy movement

---
+++ Assets/_Game/_Scripts/Enemy.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+    private bool _isFacingRight;
+
+    
+    void Start()
+    {
+        _isFacingRight = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Physics2D.Raycast(groundCheck.position, Vector2.down, 0.5f, groundLayer))
+        {
+            Debug.Log("keep walking");
+            //keep walking
+        }
+        else
+        {
+            Debug.Log("turn around");
+            _isFacingRight = !_isFacingRight;
+            var localScale = transform.localScale;
+            localScale.x = (_isFacingRight) ? 1f : -1f;
+            transform.localScale = localScale;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (_isFacingRight)
+        {
+            transform.Translate(Vector2.right * moveSpeed * Time.fixedDeltaTime);
+        }
+        else
+        {
+            transform.Translate(Vector2.left * moveSpeed * Time.fixedDeltaTime);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(groundCheck.position, groundCheck.position + (Vector3)(Vector2.down * 0.5f));
+    }
+}
